8af0f109d8d99aa88170273a912221119f9a9092
Jerry Hu <huchunhui@hotmail.com>
2024-01-12 12:58:14 -0500

    more adjustments

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 window.onload = () => {
   let tileW, tileH;
 
+  const wallDefs = [
+    { w: 39, h: 92, x: 2 },
+    { w: 54, h: 92, x: 2 },
+    { w: 39, h: 92, x: 2 },
+  ];
+  const container = document.querySelector('#walls');
+  const template = document.querySelector('#t_wall');
+  wallDefs.forEach((def) => {
+    /** @type {HTMLElement} */
+    const clone = template.content.cloneNode(true);
+    Object.entries(def).forEach(([key, val]) => {
+      clone.querySelector(`input[name=${key}]`).value = val;
+    });
+    container.appendChild(clone);
+  });
+
   const draw = () => {
     document.querySelectorAll('div.wall').forEach((wall) => {
       wall.textContent = '';
@@ -8,6 +24,8 @@ window.onload = () => {
       const vars = window.getComputedStyle(wall);
       const gap = parseInt(vars.getPropertyValue('--gap'));
       const rowShift = parseInt(vars.getPropertyValue('--rowShift')) || 3;
+      const rowDelta = parseInt(vars.getPropertyValue('--rowDelta')) || 0;
+
       const { clientWidth: wallW, clientHeight: wallH } = wall;
 
       if (wallH <= gap) {
@@ -33,10 +51,9 @@ window.onload = () => {
         return tile;
       };
 
-      const partialStart = (wallW - Math.floor(wallW / tileW) * tileW) / 2;
+      const partialStart = rowDelta + (wallW - Math.floor(wallW / tileW) * tileW) / 2;
       const rowStart = (rowIdx) => {
         const partial = (partialStart + tileW * (rowIdx % rowShift) / rowShift);
-        console.debug({ partial });
         return (partial > 0) ? partial - tileW : partial;
       };
 
@@ -44,7 +61,6 @@ window.onload = () => {
 
       // row
       for (let rowIdx = 0, rowH = tileH; rowIdx < rows; rowIdx++, rowH += tileH) {
-        console.debug('row=', rowIdx + 1);
         const tileRow = wall.appendChild(
           Object.assign(document.createElement('div'), {
             className: 'row',
@@ -54,7 +70,6 @@ window.onload = () => {
         // tiles
         const startX = rowStart(rowIdx);
         for (let rowW = 0; startX + rowW < wallW; rowW += tileW) {
-          console.debug('tile=', { startX, rowW });
           const tile = makeTile({ tileRow });
           if (rowW === 0 && startX) {
             tile.style.setProperty('--partialX', startX + 'px');
@@ -75,13 +90,15 @@ window.onload = () => {
       const w = Number(formData.get('w')) * 10;
       const h = Number(formData.get('h')) * 10;
 
+      /** @type {HTMLDivElement} */
       const wall = form.parentElement.querySelector('div.wall');
       if (wall) {
         wall.style.setProperty('--width', `${w}px`);
         wall.style.setProperty('--height', `${h}px`);
+        wall.style.setProperty('--rowDelta', `${Number(formData.get('d')) * 10}px`);
         wall.style.setProperty('--rowShift', formData.get('x'));
       } else {
-        document.body.style.setProperty('--gap', `${Number(formData.get('gap'))}px`);
+        document.body.style.setProperty('--gap', `${Number(formData.get('gap')) * 10}px`);
         document.body.style.setProperty('--tileW', `${w}px`);
         document.body.style.setProperty('--tileH', `${h}px`);
         tileW = w;
@@ -97,6 +114,7 @@ window.onload = () => {
         draw();
       };
     });
+
     setSize();
   });
 
